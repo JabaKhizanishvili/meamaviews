5041d0fc79fd6f6ecdccc078a5af6ef9253705d5
JabaKhizanishvili <xizanishvili.99@gmail.com>
2025-05-16 23:43:44 +0400

    fix vite config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-console.log('');
+// import 'swiper/swiper-bundle.min.css';
+// import 'swiper/css/bundle';
+// import '../scss/style.scss';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap-icons/font/bootstrap-icons.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './bootstrap';
+// import '../css/app.css';
+// import './functions';
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,24 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/functions.js',
+                'resources/scss/style.scss',
+                'node_modules/swiper/swiper-bundle.min.css',
+                'node_modules/bootstrap-icons/font/bootstrap-icons.css',
+
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            // შესაძლოა დაგჭირდეთ alias-ის დამატება
+            '@': '/resources',
+            'bootstrap-icons': '/node_modules/bootstrap-icons',
+            'swiper': '/node_modules/swiper',
+        },
+    },
 });
