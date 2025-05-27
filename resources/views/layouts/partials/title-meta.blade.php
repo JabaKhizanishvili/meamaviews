d9f48d474294b1bbab5a99885d386f98c4319900
JabaKhizanishvili <xizanishvili.99@gmail.com>
2025-05-27 19:57:12 +0400

    straponas font

--- resources/views/layouts/partials/title-meta.blade.php
+++ resources/views/layouts/partials/title-meta.blade.php
@@ -12,3 +12,29 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Georgian:wght@100..900&display=swap" rel="stylesheet"> 
+
+<style>
+
+.noto-serif-georgian-<uniquifier> {
+  font-family: "Noto Serif Georgian", serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+  font-variation-settings:
+    "wdth" 100;
+}
+
+
+body{
+
+  font-family: 'Noto Serif Georgian', serif !important;
+  font-weight: 400 !important; /* ნორმალური წონა */
+  font-style: normal;
+  font-optical-sizing: auto;
+}
+</style>
